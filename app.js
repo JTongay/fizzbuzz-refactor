f0f91397ee0bee415787b59929fc256ae7a83222
Joseph Tongay <dirk.tongay@gmail.com>
2016-06-14 14:05:55 -0500

    fixed number 15 bug

--- app.js
+++ app.js
@@ -44,15 +44,15 @@ $(function () {
 function fizzBuzz(max) {
   for (var i = 1; i <= max; i++) {
     console.log(i);
-    if (i % 3 === 0) {
-      console.log('fizz');
-      $('section').append('fizz').append('<br>');
+    if ((i % 3 === 0) && (i % 5 === 0)) {
+      console.log('fizzbuzz');
+      $('section').append('fizzbuzz').append('<br>');
     } else if (i % 5 === 0) {
       console.log('buzz');
       $('section').append('buzz').append('<br>');
-    } else if ((i % 3 === 0) && (i % 5 === 0)) {
-      console.log('fizzbuzz');
-      $('section').append('fizzbuzz').append('<br>');
+    } else if (i % 3 === 0) {
+      console.log('fizz');
+      $('section').append('fizz').append('<br>');
     } else {
       console.log(i);
       $('section').append(i).append('<br>');
